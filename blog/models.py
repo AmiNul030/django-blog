b30667d6dc9a425183418ebce0fbeb344fbf21b2
AmiNul030 <aminulhaquetanim30@gmail.com>
2026-03-05 03:03:44 +0600

    model code add

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    body=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    slug=models.SlugField(unique=True, max_length=200)
+    
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+     post=models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+     name=models.CharField(max_length=200)
+     body=models.TextField()
+     created_at=models.DateTimeField(auto_now_add=True)
+     
+     def __str__(self):
+         return f"{self.name} on {self.post.title}"
